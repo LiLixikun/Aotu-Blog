427bbf9709d0c3cdd492050076e12de1d1726e5d
linlei <linlei@intest.cn>
2020-09-02 13:01:38 +0800

    [CMS]调整目录结构

---
+++ CMS/src/views/base/tag/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { getTableList } from '../../../api/table'
+import './../ui.less'
+import EditableTable from './components/tableEdit'
+export default class  TagManage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: true,
+            value: 'small'
+        }
+    }
+    componentDidMount() {
+        const postData = {
+            pageSize: 10,
+            pageNum: 1,
+            query:'query'
+        }
+        getTableList(postData).then(({data})=> {
+            console.log(data)
+        })  
+
+        console.log(process.env.REACT_APP_NOT_SECRET_CODE)
+    }
+    render() {
+        return (
+            <div >
+              <EditableTable />
+            </div>
+        )
+    }
+}
